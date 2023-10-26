d5cc21e9051f9005d3758a123612bee79b33c748
钱军 <12964758+protogensis@user.noreply.gitee.com>
2023-10-26 18:01:21 +0800

    commit

--- views/ts/threetest.ts
+++ views/ts/threetest.ts
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 function initThree() {
   const canvas = document.getElementById("three") as HTMLCanvasElement;
@@ -7,21 +8,24 @@ function initThree() {
     canvas,
     antialias: true, //  打开抗锯齿
   });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
   const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xbbbbbb);
 
   return { canvas, scene, renderer };
 }
 //摄像机
-function initCamera(canvas: HTMLCanvasElement) {
+function initCamera() {
   const camera = new THREE.PerspectiveCamera(
-    75,
-    canvas.clientWidth / canvas.clientHeight,
+    100,
+    window.innerWidth / window.innerHeight,
     0.5,
-    100
+    1000
   );
 
-  camera.position.z = 2;
+  camera.position.set(1, 1, 1);
   return camera;
 }
 
@@ -36,40 +40,66 @@ function initLight() {
 
 //几何图元
 function initGeometry() {
-  const geometry = new THREE.DodecahedronGeometry(1,5);
-  const material = new THREE.MeshToonMaterial({
+  const boxgeometry = new THREE.BoxGeometry();
+  
+  const boxmaterial = new THREE.MeshToonMaterial({
     color: 0x44aa88,
   });
+  const planegeo = new THREE.PlaneGeometry(200, 100);
+  const planematerial = new THREE.MeshToonMaterial({
+    color: 0xEEC900,
+  });
+  const cube = new THREE.Mesh(boxgeometry, boxmaterial);
+  const plane = new THREE.Mesh(planegeo, planematerial);
+  cube.translateY(0.5)
+  plane.rotateX(-Math.PI/2)
 
-  const cube = new THREE.Mesh(geometry, material);
-  return cube;
+  return { cube, plane };
+}
+
+function initControl(
+  camera: THREE.PerspectiveCamera,
+  renderer: THREE.WebGLRenderer
+) {
+  let control = new OrbitControls(camera, renderer.domElement);
+  control.enableDamping = true;
+  control.minDistance = 1.4;
+  control.maxDistance = 4;
+  control.target.set(0, 0.35, 0);
+  control.update();
+  return control;
 }
 
 function run() {
   const { canvas, scene, renderer } = initThree();
-  const camera = initCamera(canvas);
+  const camera = initCamera();
   const light = initLight();
-  const cube = initGeometry();
+  const { cube, plane } = initGeometry();
+  const control = initControl(camera, renderer);
   scene.add(camera);
   scene.add(light);
   scene.add(cube);
+  scene.add(plane);
 
-  function render(time: number) {
-    time *= 0.001; // convert time to seconds
+  animate();
 
-    cube.rotation.x = time;
-    cube.rotation.y = time;
+  function animate() {
+    requestAnimationFrame(animate);
 
-    renderer.render(scene, camera);
+    control.update(); // required if damping enabled
 
-    requestAnimationFrame(render);
+    render();
   }
-  requestAnimationFrame(render);
 
-  renderer.render(scene, camera);
-  window.addEventListener("resize", () => {
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+  function render() {
     renderer.render(scene, camera);
+  }
+
+  window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
   });
 }
 
