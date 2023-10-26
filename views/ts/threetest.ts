f3c46fee1a7db03bc6a07c8716770fad8f86f77a
Protogensis <572220243@163.com>
2023-10-26 08:06:32 +0800

    commit

--- views/ts/threetest.ts
+++ views/ts/threetest.ts
@@ -12,9 +12,8 @@ function initThree() {
 
   return { canvas, scene, renderer };
 }
-
+//摄像机
 function initCamera(canvas: HTMLCanvasElement) {
-  //摄像机
   const camera = new THREE.PerspectiveCamera(
     75,
     canvas.clientWidth / canvas.clientHeight,
@@ -26,37 +25,44 @@ function initCamera(canvas: HTMLCanvasElement) {
   return camera;
 }
 
+//dengguang
+function initLight(){
+  const color = 0xFFFFFF;
+  const intensity = 3;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(-1, 2, 4);
+  return light
+}
+
 function initGeometry() {
   //几何图元
   const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+  const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
 
   const cube = new THREE.Mesh(geometry, material);
   return cube;
 }
 
-
-
 function run() {
   const { canvas, scene, renderer } = initThree();
   const camera = initCamera(canvas);
+  const light = initLight()
   const cube = initGeometry();
-  scene.add(camera)
+  scene.add(camera);
+  scene.add(light)
   scene.add(cube);
 
-  function render( time ) {
-
+  function render(time: number) {
     time *= 0.001; // convert time to seconds
-  
+
     cube.rotation.x = time;
     cube.rotation.y = time;
-  
-    renderer.render( scene, camera );
-  
-    requestAnimationFrame( render );
-  
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
   }
-  requestAnimationFrame(render)
+  requestAnimationFrame(render);
 
   renderer.render(scene, camera);
   window.addEventListener("resize", () => {
