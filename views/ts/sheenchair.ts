1ad63efe2ca12f5c19b8d805f36debc2634db743
Protogensis <572220243@163.com>
2023-10-28 07:49:44 +0800

    commit

--- views/ts/sheenchair.ts
+++ views/ts/sheenchair.ts
@@ -6,24 +6,43 @@ import { RoomEnvironment } from "three/addons/environments/RoomEnvironment.js";
 
 import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 
-let camera, scene, renderer, controls;
 
 
 
-function init() {
-  const container = document.createElement("div");
-  document.body.appendChild(container);
+function initThree() {
+  const canvas = document.getElementById('three') as HTMLCanvasElement;
+  const scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(
+  const renderer = new THREE.WebGLRenderer({ 
+    canvas,
+    antialias: true 
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 1;
+  const environment = new RoomEnvironment(renderer);
+  const pmremGenerator = new THREE.PMREMGenerator(renderer);
+
+  scene.background = new THREE.Color(0xbbbbbb);
+  scene.environment = pmremGenerator.fromScene(environment).texture;
+
+  return { scene, renderer }
+}
+
+function initCamera(scene: THREE.Scene) {
+  const camera = new THREE.PerspectiveCamera(
     45,
     window.innerWidth / window.innerHeight,
     0.1,
     20
   );
   camera.position.set(-0.75, 0.7, 1.25);
+  scene.add(camera)
+  return camera
+}
 
-  scene = new THREE.Scene();
-
+function initModel(scene: THREE.Scene) {
   // model
 
   new GLTFLoader()
@@ -32,7 +51,8 @@ function init() {
       scene.add(gltf.scene);
 
       const object = gltf.scene.getObjectByName("SheenChair_fabric");
-      console.log(gltf)
+      if(!object)
+        throw new Error()
 
       const gui = new GUI();
       object.material.sheen = 0
@@ -40,49 +60,45 @@ function init() {
       gui.open();
     });
 
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.toneMapping = THREE.ACESFilmicToneMapping;
-  renderer.toneMappingExposure = 1;
-  container.appendChild(renderer.domElement);
-
-  const environment = new RoomEnvironment(renderer);
-  const pmremGenerator = new THREE.PMREMGenerator(renderer);
-
-  scene.background = new THREE.Color(0xbbbbbb);
-  scene.environment = pmremGenerator.fromScene(environment).texture;
-
-  controls = new OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true;
-  controls.minDistance = 1;
-  controls.maxDistance = 10;
-  controls.target.set(0, 0.35, 0);
-  controls.update();
-
-  window.addEventListener("resize", onWindowResize);
 }
 
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
+function initControl(camera: THREE.Camera, renderer: THREE.WebGLRenderer) {
+  const control = new OrbitControls(camera, renderer.domElement);
+  control.enableDamping = true;
+  control.minDistance = 1;
+  control.maxDistance = 10;
+  control.target.set(0, 0.35, 0);
+  control.update();
+  return control
 }
 
-//
+function run() {
+  const { scene, renderer } = initThree()
+  const camera = initCamera(scene)
+  initModel(scene)
+  const control = initControl(camera, renderer)
+  window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-function animate() {
-  requestAnimationFrame(animate);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  });
+  animate();
+  function animate() {
+    requestAnimationFrame(animate);
 
-  controls.update(); // required if damping enabled
+    control.update(); // required if damping enabled
 
-  render();
+    render();
+  }
+
+  function render() {
+    renderer.render(scene, camera);
+  }
 }
 
-function render() {
-  renderer.render(scene, camera);
-}
 
-init();
-animate();
+
+run()
+
+
