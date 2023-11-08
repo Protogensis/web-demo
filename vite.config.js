c77cf084ee0f1963320cbe70a5eb31c7fb7b5a63
钱军 <12964758+protogensis@user.noreply.gitee.com>
2023-11-08 10:58:04 +0800

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base:'./',
@@ -6,4 +7,25 @@ export default defineConfig({
   preview: {
     port: 8080,
   },
+  build:{
+    rollupOptions:{
+      input:{
+        main: resolve(__dirname, 'index.html'),
+        helloweb: resolve(__dirname, 'views/helloweb.html'),
+        xhxtx: resolve(__dirname, 'views/xhxtx.html'),
+        dzjtx: resolve(__dirname, 'views/dzjtx.html'),
+        tbfgtx: resolve(__dirname, 'views/tbfgtx.html'),
+        tpkp: resolve(__dirname, 'views/tpkp.html'),
+        gdsctx: resolve(__dirname, 'views/gdsctx.html'),
+        klsflbj: resolve(__dirname, 'views/klsflbj.html'),
+        echarttest: resolve(__dirname, 'views/echarttest.html'),
+        threetest: resolve(__dirname, 'views/threetest.html'),
+        gltfloader: resolve(__dirname, 'views/gltfloader.html'),
+        sheenchair: resolve(__dirname, 'views/sheenchair.html'),
+        litiqiu: resolve(__dirname, 'views/litiqiu.html'),
+        nhdzt: resolve(__dirname, 'views/nhdzt.html'),
+      }
+    }
+  }
+  
 })
