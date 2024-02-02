f6fcc0d151fce61f5806cf80a513955a5faf1394
Protogensis <572220243@163.com>
2024-02-02 15:51:23 +0800

    🦄 refactor:
    移除不需要的依赖包，增加vue和vue-router的依赖

---
+++ src/routes/index.ts
@@ -0,0 +1,25 @@
+import HelloWeb from '../../views/helloweb.vue'
+import xhxtx from '../../views/xhxtx.vue'
+import dzjtx from '../../views/dzjtx.vue'
+import tbfgtx from '../../views/tbfgtx.vue'
+import tpkp from '../../views/tpkp.vue'
+import gdsctx from '../../views/gdsctx.vue'
+import klsflbj from '../../views/klsflbj.vue'
+import threetest from '../../views/threetest.vue'
+import sheetchair from '../../views/sheenchair.vue'
+import litiqiu from '../../views/litiqiu.vue'
+
+const routes = [
+  { path: '/', component: HelloWeb },
+  { path: '/xhxtx', component: xhxtx },
+  { path: '/dzjtx', component: dzjtx },
+  { path: '/tbfgtx', component: tbfgtx },
+  { path: '/tpkp', component: tpkp },
+  { path: '/gdsctx', component: gdsctx },
+  { path: '/klsflbj', component: klsflbj },
+  { path: '/threetest', component: threetest },
+  { path: '/sheetchair', component: sheetchair },
+  { path: '/litiqiu', component: litiqiu },
+]
+
+export default routes
--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,6 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base:'./',
-  
-  preview: {
-    port: 8080,
-  },
-  build:{
-    rollupOptions:{
-      input:{
-        main: resolve(__dirname, 'index.html'),
-        helloweb: resolve(__dirname, 'views/helloweb.html'),
-        xhxtx: resolve(__dirname, 'views/xhxtx.html'),
-        dzjtx: resolve(__dirname, 'views/dzjtx.html'),
-        tbfgtx: resolve(__dirname, 'views/tbfgtx.html'),
-        tpkp: resolve(__dirname, 'views/tpkp.html'),
-        gdsctx: resolve(__dirname, 'views/gdsctx.html'),
-        klsflbj: resolve(__dirname, 'views/klsflbj.html'),
-        echarttest: resolve(__dirname, 'views/echarttest.html'),
-        threetest: resolve(__dirname, 'views/threetest.html'),
-        gltfloader: resolve(__dirname, 'views/gltfloader.html'),
-        sheenchair: resolve(__dirname, 'views/sheenchair.html'),
-        litiqiu: resolve(__dirname, 'views/litiqiu.html'),
-        nhdzt: resolve(__dirname, 'views/nhdzt.html'),
-      }
-    }
-  }
-  
+  plugins:[vue()],
 })
