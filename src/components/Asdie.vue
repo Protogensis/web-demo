04867c353cb4814272275b4aaf914117ec5458c6
Protogensis <572220243@163.com>
2024-02-02 15:53:12 +0800

    🦄 refactor:
    用Aside侧边栏文件替代原本html文件内的侧边栏

---
+++ src/components/Asdie.vue
@@ -0,0 +1,36 @@
+/**
+ 侧边栏
+ */
+<template>
+  <aside>
+    <input aria-label="search" type="search" id="search" />
+    <RouterLink class="sample" to="/">HelloWeb</RouterLink>
+    <RouterLink class="sample" to="/xhxtx">下划线特效</RouterLink >
+    <RouterLink class="sample" to="/dzjtx">打字机特效</RouterLink >
+    <RouterLink class="sample" to="/tbfgtx">图标发光特效</RouterLink >
+    <RouterLink class="sample" to="/tpkp">躺平卡片</RouterLink >
+    <RouterLink class="sample" to="/gdsctx">滚动视差特效</RouterLink >
+    <RouterLink class="sample" to="/klsflbj">可拉伸分栏布局</RouterLink >
+
+    <h1>Three.js</h1>
+    <!-- <RouterLink class="sample" to="/ifcloader">ifc加载器</RouterLink > -->
+    <RouterLink class="sample" to="/threetest">threetest</RouterLink >
+    <RouterLink class="sample" to="/gltfloader">gltfloader</RouterLink >
+    <RouterLink class="sample" to="/sheenchair">threejs-椅子</RouterLink >
+    <RouterLink class="sample" to="/litiqiu">threejs-立体球</RouterLink >
+    <h1>from CSS灵感</h1>
+    <RouterLink class="sample" to="/nhdzt">霓虹灯字体</RouterLink >
+    <h1>CSS友情连接</h1>
+    <div class="otherside">
+      <a class="other" href="https://codepen.io/" target="_blank" rel="noopener noreferrer">code pen</a>
+      <a class="other" href="https://chokcoco.github.io/CSS-Inspiration/#/" target="_blank"
+        rel="noopener noreferrer">CSS灵感</a>
+      <a class="other" href="https://juejin.cn/post/6921481535774261261" target="_blank"
+        rel="noopener noreferrer">132个CSS特效</a>
+      <a class="other" href="https://getcssscan.com/css-box-shadow-examples?ref=producthunt" target="_blank"
+        rel="noopener noreferrer">css阴影</a>
+      <a class="other" href="https://juejin.cn/post/6982363593241002014" target="_blank"
+        rel="noopener noreferrer">好玩的CSS</a>
+    </div>
+  </aside>
+</template>
